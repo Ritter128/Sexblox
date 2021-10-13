2692faf9aeb44a0b18728e21738e0707d85f9494
Evan Weeks <evanweeks500@gmail.com>
2021-10-13 17:27:50 -0400

    Created triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm.hpp>
+#include <iostream>
+
+struct Vertex
+{
+    glm::vec3 position;
+};
 
 int main(void)
 {
@@ -19,11 +27,37 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGL())
+    {
+        std::cout << "Failed to load GLAD\n";
+        return -1;
+    }
+
+    Vertex vertices[] = {
+        glm::vec3(-0.5f,-0.5f, 0.0f),
+        glm::vec3(0.5, -0.5f, 0.0f),
+        glm::vec3(-0.5, 0.5f, 0.0f),
+    };
+
+    unsigned int vertexArrayID;
+    glGenVertexArrays(1, &vertexArrayID);
+    glBindVertexArray(vertexArrayID);
+
+    unsigned int vertexBufferID;
+    glGenBuffers(1, &vertexBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex, position));
+    glEnableVertexArrayAttrib(vertexArrayID, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-       // glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
